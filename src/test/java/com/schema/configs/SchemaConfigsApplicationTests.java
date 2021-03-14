dadd70b12504a7d973096961732746e31c3fc246
durgeshg <durgesh.gaur@brambles.com>
2021-03-14 15:32:11 +0530

    initial commit

---
+++ src/test/java/com/schema/configs/SchemaConfigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schema.configs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaConfigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
